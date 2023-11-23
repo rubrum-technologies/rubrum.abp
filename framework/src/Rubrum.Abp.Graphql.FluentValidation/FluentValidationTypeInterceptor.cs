e967730a4cbd6ce757e666a0b42967772511c0c6
Artem Sharypov <79115666058@ya.ru>
2023-11-24 00:26:48 +0300

    Graphql dataloader fix (#21)

--- framework/src/Rubrum.Abp.Graphql.FluentValidation/FluentValidationTypeInterceptor.cs
+++ framework/src/Rubrum.Abp.Graphql.FluentValidation/FluentValidationTypeInterceptor.cs
@@ -37,7 +37,7 @@ public class FluentValidationTypeInterceptor : TypeInterceptor
                 continue;
             }
 
-            field.Description = string.Join($",{Environment.NewLine} ", values);
+            field.Description = string.Join($",{Environment.NewLine}", values);
         }
     }
 
