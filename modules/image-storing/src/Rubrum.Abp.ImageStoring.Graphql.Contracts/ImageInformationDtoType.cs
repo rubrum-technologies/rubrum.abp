2eddb9487d4ee98d3d39c3ede97565a902bcb5b0
Artem Sharypov <79115666058@ya.ru>
2023-12-07 21:29:58 +0300

    Create ImageInformationDtoType.cs

---
+++ modules/image-storing/src/Rubrum.Abp.ImageStoring.Graphql.Contracts/ImageInformationDtoType.cs
@@ -0,0 +1,21 @@
+﻿using HotChocolate.Types;
+using Rubrum.Abp.Graphql.Types;
+
+namespace Rubrum.Abp.ImageStoring;
+
+public class ImageInformationDtoType : ObjectType<ImageInformationDto>, IGraphqlType
+{
+    protected override void Configure(IObjectTypeDescriptor<ImageInformationDto> descriptor)
+    {
+        descriptor
+            .Field("url")
+            .Resolve(context =>
+            {
+                var parent = context.Parent<ImageInformationDto>();
+                var dataLoader = context.Service<IImageUrlByIdDataLoader>();
+
+                return dataLoader.LoadAsync(parent.Id, context.RequestAborted);
+            })
+            .Type<NonNullType<StringType>>();
+    }
+}
