527b82dd71d25416d692c858728286cfc1e645a1
Artem Sharypov <79115666058@ya.ru>
2023-12-30 15:17:58 +0300

    Fix bugs and improvements

--- modules/image-storing/src/Rubrum.Abp.ImageStoring.Application/ImageAppService.cs
+++ modules/image-storing/src/Rubrum.Abp.ImageStoring.Application/ImageAppService.cs
@@ -7,27 +7,17 @@ using Volo.Abp.Threading;
 
 namespace Rubrum.Abp.ImageStoring;
 
-public class ImageAppService : ApplicationService, IImageAppService
+public class ImageAppService(
+    IImageContainer imageContainer,
+    IImageMapper mapper,
+    ICancellationTokenProvider cancellationTokenProvider)
+    : ApplicationService, IImageAppService
 {
-    private readonly IImageContainer _imageContainer;
-    private readonly IImageMapper _mapper;
-    private readonly ICancellationTokenProvider _cancellationTokenProvider;
-
-    public ImageAppService(
-        IImageContainer imageContainer,
-        IImageMapper mapper,
-        ICancellationTokenProvider cancellationTokenProvider)
-    {
-        _imageContainer = imageContainer;
-        _mapper = mapper;
-        _cancellationTokenProvider = cancellationTokenProvider;
-    }
-
     public async Task<IRemoteStreamContent?> DownloadAsync(Guid id)
     {
-        var cancellationToken = _cancellationTokenProvider.Token;
+        var cancellationToken = cancellationTokenProvider.Token;
 
-        var file = await _imageContainer.GetOrNullAsync(id, cancellationToken);
+        var file = await imageContainer.GetOrNullAsync(id, cancellationToken);
 
         return file is null
             ? null
@@ -38,30 +28,30 @@ public class ImageAppService : ApplicationService, IImageAppService
     {
         await CheckPolicyAsync(ImageStoringPermissions.Images.Upload);
 
-        var cancellationToken = _cancellationTokenProvider.Token;
+        var cancellationToken = cancellationTokenProvider.Token;
 
-        await _imageContainer.UpdateAsync(id, file.GetStream(), cancellationToken);
+        await imageContainer.UpdateAsync(id, file.GetStream(), cancellationToken);
     }
 
     public async Task<ImageInformationDto> UploadAsync(UploadImageInput input)
     {
         await CheckPolicyAsync(ImageStoringPermissions.Images.Upload);
 
-        var cancellationToken = _cancellationTokenProvider.Token;
+        var cancellationToken = cancellationTokenProvider.Token;
 
         var id = GuidGenerator.Create();
         var bytes = await input.Content.GetStream().GetAllBytesAsync(cancellationToken);
         var file = new ImageFile(id, bytes, input.Content.FileName, input.Tag, input.IsDisposable);
-        await _imageContainer.CreateAsync(file, cancellationToken);
+        await imageContainer.CreateAsync(file, cancellationToken);
 
-        return _mapper.Map(file);
+        return mapper.Map(file);
     }
 
     public async Task<ListResultDto<ImageInformationDto>> UploadAsync(UploadImagesInput input)
     {
         await CheckPolicyAsync(ImageStoringPermissions.Images.Upload);
 
-        var cancellationToken = _cancellationTokenProvider.Token;
+        var cancellationToken = cancellationTokenProvider.Token;
         var result = new List<ImageInformationDto>();
 
         foreach (var content in input.Contents)
@@ -69,18 +59,20 @@ public class ImageAppService : ApplicationService, IImageAppService
             var id = GuidGenerator.Create();
             var bytes = await content.GetStream().GetAllBytesAsync(cancellationToken);
             var file = new ImageFile(id, bytes, content.FileName, input.Tag, input.IsDisposable);
-            await _imageContainer.CreateAsync(file, cancellationToken);
+            await imageContainer.CreateAsync(file, cancellationToken);
 
-            result.Add(_mapper.Map(file));
+            result.Add(mapper.Map(file));
         }
 
         return new ListResultDto<ImageInformationDto>(result);
     }
 
-    public Task DeleteAsync(Guid id)
+    public async Task DeleteAsync(Guid id)
     {
-        var cancellationToken = _cancellationTokenProvider.Token;
+        await CheckPolicyAsync(ImageStoringPermissions.Images.Delete);
 
-        return _imageContainer.DeleteAsync(id, cancellationToken);
+        var cancellationToken = cancellationTokenProvider.Token;
+
+        await imageContainer.DeleteAsync(id, cancellationToken);
     }
 }
