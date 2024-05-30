34ae32fd5ae5946dd74e91a4296ab5dc5d9a7fc5
Artem Sharypov <79115666058@ya.ru>
2024-05-30 21:32:06 +0300

    Add configuration filter graphql (#40)

--- framework/src/Rubrum.Abp.Graphql/RubrumAbpGraphqlOptions.cs
+++ framework/src/Rubrum.Abp.Graphql/RubrumAbpGraphqlOptions.cs
@@ -1,6 +1,10 @@
-﻿namespace Rubrum.Abp.Graphql;
+﻿using HotChocolate.Data.Filters;
+
+namespace Rubrum.Abp.Graphql;
 
 public class RubrumAbpGraphqlOptions
 {
+    public List<Action<IFilterConventionDescriptor>> FilterBuildActions { get; set; } = [];
+
     public bool EnableGlobalObjectIdentification { get; set; } = true;
 }
